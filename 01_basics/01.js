22a7bf9c740c3aed86bc863b3f17106b7ef72510
looyd-droid <lloydtv923@gmail.com>
2026-07-28 13:51:03 +0000

    basics

---
+++ 01_basics/01.js
@@ -0,0 +1,14 @@
+let a;
+console.log(a)
+
+a=5;
+console.log(a)
+
+const name = new String ('Shreyas')
+console.log(name.charAt(3));
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase())
+
+console.log(name.anchor())
