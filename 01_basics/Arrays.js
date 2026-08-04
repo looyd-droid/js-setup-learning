976bfaf22acb75bbb6d56a32dd424115abb6428f
looyd-droid <lloydtv923@gmail.com>
2026-08-04 14:49:44 +0000

    Arrays

---
+++ 01_basics/Arrays.js
@@ -0,0 +1,41 @@
+// const myArr = [2,3,4,5,6]
+
+// console.log(myArr);
+
+// myArr.push(7);
+// console.log(myArr);
+
+// myArr.pop(3);
+
+// console.log(myArr);
+
+// const fruits = ["Banana","Orange","Apple","Mango"];
+// fruits.reverse();
+// console.log(fruits);
+
+// fruits.sort();
+// console.log(fruits);
+
+// const points = [43,32,1,56,87,67]
+// console.log(points.sort())
+
+// const roll_no = [1,2,3,4,5,6,7,8,9]
+// const marks =[12,13,14,11,10,19,18,11,12]
+
+// console.log(roll_no)
+// console.log(marks.sort())
+
+const marvel_heros = ["Spiderman","Thor","IronMan"]
+const dc_heros = ["Superman","Batman","flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros)
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
